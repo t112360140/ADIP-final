afbfa6df137d46c03d12bc1cf39f2b45b3a3edc5
t112360140 <155874754+t112360140@users.noreply.github.com>
2025-12-13 12:52:45 +0800

    add mouse select

--- js/image_editor.js
+++ js/image_editor.js
@@ -9,14 +9,17 @@ const image_editor_canvas_div = document.getElementById('image-editor-canvas-div
 const image_editor_canvas = document.getElementById('image-editor-canvas');
 const image_editor_mask_canvas = document.getElementById('image-editor-mask-canvas');
 
-
+let mouseData={};
 image_editor_canvas_div.addEventListener('mousedown', (event)=>{
     if(image_editing_data.img){
         const pos={
             x: event.offsetX*(image_editing_data.img.width/image_editor_canvas_div.offsetWidth),
             y:event.offsetY*(image_editing_data.img.height/image_editor_canvas_div.offsetHeight),
         };
-        console.log(pos);
+        mouseData.isDown=true;
+        mouseData.start=pos;
+        image_editor_mask_canvas.height=image_editor_canvas.height;
+        image_editor_mask_canvas.width=image_editor_canvas.width;
     }
 });
 image_editor_canvas_div.addEventListener('mouseup', (event)=>{
@@ -25,9 +28,24 @@ image_editor_canvas_div.addEventListener('mouseup', (event)=>{
             x: event.offsetX*(image_editing_data.img.width/image_editor_canvas_div.offsetWidth),
             y:event.offsetY*(image_editing_data.img.height/image_editor_canvas_div.offsetHeight),
         };
-        console.log(pos);
+        mouseData.isDown=false;
+        mouseData.end=pos;
     }
 });
+image_editor_canvas_div.addEventListener('mousemove', (event)=>{
+    if(image_editing_data.img){
+        const pos={
+            x: event.offsetX*(image_editing_data.img.width/image_editor_canvas_div.offsetWidth),
+            y:event.offsetY*(image_editing_data.img.height/image_editor_canvas_div.offsetHeight),
+        };
+        if(mouseData.isDown&&mouseData.start){
+            const ctx=image_editor_mask_canvas.getContent('2d');
+            ctx.strokeStyle='#ff0000';
+            ctx.clearRect(0, 0, image_editor_mask_canvas.width, image_editor_mask_canvas.height);
+            ctx.strokeRect(mouseData.start.x, mouseData.start.y, pos.x-mouseData.start.x, pos.y-mouseData.start.y);
+        }
+    }
+}); 
 
 function setEditImage(image, mode){
     if(!MODE[mode]) return;
